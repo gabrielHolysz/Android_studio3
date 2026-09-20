6d0c468be039a0afad3b585053aa8b50d979837d
Gabriel Hołysz <gabrysholysz@gmail.com>
2026-09-20 15:15:14 +0200

    dodanie methody by wszytko działało

--- app/src/main/java/com/example/gabrysholysz/MainActivity.java
+++ app/src/main/java/com/example/gabrysholysz/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.gabrysholysz;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etName;
+    private Button btnGreet;
+    private TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        etName = findViewById(R.id.etName);
+        btnGreet = findViewById(R.id.btnGreet);
+        tvResult = findViewById(R.id.tvResult);
+
+        // co ma się stać po kliknięciu
+        btnGreet.setOnClickListener(v -> greet());
+    }
+
+    private void greet() {
+        String name = etName.getText().toString().trim();
+
+        if (name.isEmpty()) {
+            Toast.makeText(this, R.string.error_empty_name, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        tvResult.setText(getString(R.string.greeting, name));
     }
 }
